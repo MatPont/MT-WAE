14bc10309464c8e92045eb0833c6f1059a191415
Mathieu Pont <34889480+MatPont@users.noreply.github.com>
2023-03-31 12:28:44 +0200

    Update automataSpeedUp.sh

--- scripts/automataSpeedUp.sh
+++ scripts/automataSpeedUp.sh
@@ -2,7 +2,7 @@
 
 lrMT=(0.05 0.0025 0.0025 0.1 0.025 0.1 0.001 0.025 0.005 0.01 0.01 0.025)
 lrPD=(0.5 0.1 0.001 0.05 10 100 0.005 0.00025 0.0025 0.005 0.01 0.5)
-names=(starting isabel sea vortex particular cloud astroT impact volcanic astro3D earthquake darkSky4)
+names=(starting isabel sea vortex particular cloud astroT impact volcanic astro3D earthquake darkSky)
 lbsl=(10 10 5 10 10 10 10 10 10 10 10 10)
 miMT=(1000 500 100 500 1500 500 500 500 500 100 500 500)
 miPD=(1000 200 100 500 1000 500 500 500 500 100 500 500)
